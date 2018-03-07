8ff46923a81895fab156eb6c0224f6279f91647c
walidoa97 <walid.omar.ag@gmail.com>
2018-03-07 18:35:00 -0400

    Create notas.h

---
+++ notas.h
@@ -0,0 +1,12 @@
+#pragma once
+class ciclofor
+{
+private:
+	float np,p1,p2,p3,ef;
+	
+public:
+	ciclofor(void);
+	~ciclofor(void);
+	float notas(float np,float p1,float p2,float p3,float ef);
+};
+
