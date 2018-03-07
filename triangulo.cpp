2d7e8c143c3bd03c3ddf935d628a5147abe1f782
walidoa97 <walid.omar.ag@gmail.com>
2018-03-07 18:46:42 -0400

    Create triangulo.cpp

---
+++ triangulo.cpp
@@ -0,0 +1,17 @@
+#include "StdAfx.h"
+#include "area.h"
+
+area::area(void)
+{
+}
+
+area::~area(void)
+{
+}
+
+float area::calcular_areatriangulo(float b,float h)
+{
+	float x;
+	x=(v*a)/2;
+	return x;
+}
