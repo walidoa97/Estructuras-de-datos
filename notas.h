cab62eef4fc51d8093fad6bd87981ece52d19c23
walidoa97 <walid.omar.ag@gmail.com>
2018-03-07 20:18:32 -0400

    Update notas.h

--- notas.h
+++ notas.h
@@ -1,12 +1,12 @@
 #pragma once
-class ciclofor
+class notas
 {
 private:
 	float np,p1,p2,p3,ef;
 	
 public:
-	ciclofor(void);
-	~ciclofor(void);
+	notas(void);
+	~notas(void);
 	float notas(float np,float p1,float p2,float p3,float ef);
 };
 
